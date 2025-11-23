de1b980b6fe00bd0bccbddfdd2dc31df00fb0630
Elena Chustova <chustova.lena@gmail.com>
2025-11-23 17:33:10 +0300

    create crud views for status app

--- task_manager/status/views.py
+++ task_manager/status/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from .models import Status
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from .forms import CustomStatusCreationForm
+from django.contrib import messages
+
 
 # Create your views here.
+class StatusIndexView(ListView):
+    model = Status
+    ordering = ['id']
+    template_name = 'status/index.html'
+    context_object_name = 'statuses'
+    paginate_by = 15
+
+
+class StatusCreateView(LoginRequiredMixin, CreateView):
+    model = Status
+    form_class = CustomStatusCreationForm
+    template_name = "status/create.html"
+    success_url = reverse_lazy("statuses:index")
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Статус успешно добавлен')
+        return super().form_valid(form)
+
+
+class StatusUpdateView(LoginRequiredMixin, UpdateView):
+    model = Status
+    form_class = CustomStatusCreationForm
+    template_name = 'status/update.html'
+    success_url = reverse_lazy('statuses:index')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Статус успешно изменен')
+        return super().form_valid(form)
+
+
+class StatusDeleteView(LoginRequiredMixin, DeleteView):
+    model = Status
+    template_name = 'status/delete.html'
+    success_url = reverse_lazy('statuses:index')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Статус успешно удален')
+        return super().form_valid(form)
