508fc8b537e6f6814e2aa0f4007b159f6139fe2f
Elena Chustova <chustova.lena@gmail.com>
2025-11-23 16:11:31 +0300

    add login/logout and home views

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 
 class HomePageView(TemplateView):
     template_name = 'index.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['who'] = 'World'
-        return context
+
+class CustomLoginView(LoginView):
+    redirect_authenticated_user = True
+    template_name = "registration/login.html"
+
+    def form_valid(self, form):
+        messages.success(self.request, "Вы успешно вошли в аккаунт!")
+        return super().form_valid(form)
+
+
+class CustomLogoutView(LogoutView):
+    next_page = "login"
+
+    def dispatch(self, request, *args, **kwargs):
+        messages.info(request, "Вы успешно вышли из аккаунта")
+        return super().dispatch(request, *args, **kwargs)
