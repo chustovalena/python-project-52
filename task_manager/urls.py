2ada06d82caf35786116e364f5b9802e875f8cb4
Elena Chustova <chustova.lena@gmail.com>
2025-11-23 16:11:02 +0300

    add login/logout, home, users include paths

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from django.urls import path
-from .views import HomePageView
+from django.urls import path, include
+from .views import HomePageView, CustomLoginView, CustomLogoutView
 
 urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('users/', include(('task_manager.user.urls', 'users'), namespace='users')),
     path('admin/', admin.site.urls),
-    path('', HomePageView.as_view())
 ]
 
