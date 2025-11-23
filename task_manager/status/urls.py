ff7c9a60f7ea3cced3ce5343860f4ce83013197d
Elena Chustova <chustova.lena@gmail.com>
2025-11-23 17:34:00 +0300

    add index, create, update, delete urls for status app

---
+++ task_manager/status/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import StatusIndexView, StatusCreateView, StatusUpdateView, StatusDeleteView
+
+
+urlpatterns = [
+    path('', StatusIndexView.as_view(), name='index'),
+    path('create/', StatusCreateView.as_view(), name='create'),
+    path('<int:pk>/update/', StatusUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', StatusDeleteView.as_view(), name='delete'),
+]
