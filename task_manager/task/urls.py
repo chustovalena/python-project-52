c86ccd6da7884236bd1f586dc9d98cbf85ad4473
Elena Chustova <chustova.lena@gmail.com>
2025-11-25 15:05:22 +0300

    add urls paths for task app

---
+++ task_manager/task/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import TaskIndexView, TaskCreateView, TaskUpdateView, TaskUpdateView, TaskDetailView
+
+
+urlpatterns = [
+    path('', TaskIndexView.as_view(), name='index'),
+    path('create/', TaskCreateView.as_view(), name='create'),
+    path('<int:pk>/', TaskDetailView.as_view(), name='detail'),
+    path('<int:pk>/update', TaskUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete', TaskUpdateView.as_view(), name='delete'),
+]
