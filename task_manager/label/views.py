2211ab29f7ea67f9a99d2066bbf2eb0b96bbccf2
Elena Chustova <chustova.lena@gmail.com>
2025-11-25 18:34:47 +0300

    add views to label app

--- task_manager/label/views.py
+++ task_manager/label/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Label
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from .forms import LabelForm
 
-# Create your views here.
+class LabelIndexView(LoginRequiredMixin, ListView):
+    model = Label
+    context_object_name = 'labels'
+    template_name = 'label/index.html'
+    ordering = ['id']
+    paginate_by = 15
+
+
+class LabelCreateView(LoginRequiredMixin, CreateView):
+    model = Label
+    form_class = LabelForm
+    success_url = reverse_lazy('labels:index')
+    template_name = 'label/create.html'
+
+
+class LabelUpdateView(LoginRequiredMixin, UpdateView):
+    model = Label
+    form_class = LabelForm
+    success_url = reverse_lazy('labels:index')
+    template_name = 'label/update.html'
+
+
+class LabelDeleteView(LoginRequiredMixin, DeleteView):
+    model = Label
+    template_name = 'label/delete.html'
+    success_url = reverse_lazy('labels:index')
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if self.object.tasks.exists():
+            return redirect(self.success_url)
+        return super().post(request, *args, **kwargs)
+
+
+
