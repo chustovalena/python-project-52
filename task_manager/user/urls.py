f7460493e46e47a4c679599769757a70d7526e7c
Elena Chustova <chustova.lena@gmail.com>
2025-11-27 09:10:12 +0300

    add detail path for user urlpatterns

--- task_manager/user/urls.py
+++ task_manager/user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import UserIndexView, UserCreateView, UserUpdateView, UserDeleteView
+from .views import UserIndexView, UserCreateView, UserUpdateView, UserDeleteView, UserDetailView
 
 
 urlpatterns = [
     path("", UserIndexView.as_view(), name='index'),
     path('create/', UserCreateView.as_view(), name='create'),
+    path('<int:pk>', UserDetailView.as_view(), name='detail'),
     path('<int:pk>/update/', UserUpdateView.as_view(), name='update'),
     path('<int:pk>/delete/', UserDeleteView.as_view(), name='delete'),
 ]
