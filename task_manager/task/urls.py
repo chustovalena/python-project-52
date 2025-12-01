0d54acf820881d3b5bb10b96434ef6877190a972
Elena Chustova <chustova.lena@gmail.com>
2025-12-01 09:54:59 +0300

    fix delete path

--- task_manager/task/urls.py
+++ task_manager/task/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TaskIndexView, TaskCreateView, TaskUpdateView, TaskUpdateView, TaskDetailView
+from .views import TaskIndexView, TaskCreateView, TaskUpdateView, TaskDeleteView, TaskDetailView
 
 
 urlpatterns = [
@@ -7,5 +7,5 @@ urlpatterns = [
     path('create/', TaskCreateView.as_view(), name='create'),
     path('<int:pk>/', TaskDetailView.as_view(), name='detail'),
     path('<int:pk>/update', TaskUpdateView.as_view(), name='update'),
-    path('<int:pk>/delete', TaskUpdateView.as_view(), name='delete'),
+    path('<int:pk>/delete', TaskDeleteView.as_view(), name='delete'),
 ]
