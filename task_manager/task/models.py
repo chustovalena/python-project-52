a5e1cba0475ea37733ceb446f9bb89fdfb1385d8
Elena Chustova <chustova.lena@gmail.com>
2025-12-05 17:25:35 +0300

    add unique=True for name

--- task_manager/task/models.py
+++ task_manager/task/models.py
@@ -7,13 +7,13 @@ from task_manager.label.models import Label
 User = get_user_model()
 
 class Task(models.Model):
-    name = models.CharField(max_length=255, blank=False, null=False)
+    name = models.CharField(max_length=255, blank=False, null=False, unique=True)
     description = models.TextField(blank=True)
     status = models.ForeignKey(Status, on_delete=models.PROTECT, verbose_name='Статус')
     author = models.ForeignKey(User, on_delete=models.PROTECT, related_name='created_tasks')
     executor = models.ForeignKey(User, on_delete=models.PROTECT, related_name='tasks', null=True, blank=True, verbose_name='Исполнитель')
     created_at = models.DateTimeField(auto_now_add=True)
-    labels = models.ManyToManyField(Label, related_name='tasks', null=True, verbose_name='Метки')
+    labels = models.ManyToManyField(Label, related_name='tasks', null=True, blank=True, verbose_name='Метки')
 
     def __str__(self):
         return self.name
