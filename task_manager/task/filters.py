8e71406dbfb16eabd7e00f5a3a29ec806ef91832
Elena Chustova <chustova.lena@gmail.com>
2025-12-09 15:23:21 +0300

    update filter

--- task_manager/task/filters.py
+++ task_manager/task/filters.py
@@ -2,6 +2,7 @@ import django_filters
 from django import forms
 from .models import Task
 from django.utils.translation import gettext_lazy as _
+from task_manager.label.models import Label
 
 
 class TaskFilter(django_filters.FilterSet):
@@ -12,6 +13,11 @@ class TaskFilter(django_filters.FilterSet):
         widget=forms.CheckboxInput(attrs={'class': 'form-check-input'})
     )
 
+    labels = django_filters.ModelChoiceFilter(
+        queryset=Label.objects.all(),
+        label=_('Label'),
+        widget=forms.Select(attrs={'class': 'form-select form-control'})
+    )
     class Meta:
         model = Task
         fields = ['status', 'executor', 'labels']
@@ -26,9 +32,8 @@ class TaskFilter(django_filters.FilterSet):
         self.form.fields['executor'].widget.attrs.update({
             'class': 'form-select form-control'
         })
-        self.form.fields['labels'].widget = forms.Select(attrs={
-            'class': 'form-select form-control'
-        })
+
+        self.form.fields['executor'].label_from_instance = lambda obj: obj.get_full_name() or obj.username
 
     def filter_self_tasks(self, queryset, name, value):
         if value:
