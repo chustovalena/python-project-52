0d2a0135af07b2e7015501281bf389bf8fc65671
Elena Chustova <chustova.lena@gmail.com>
2025-12-09 13:19:22 +0300

    add missing / at the and of urls path

--- task_manager/label/urls.py
+++ task_manager/label/urls.py
@@ -5,6 +5,6 @@ from .views import LabelIndexView, LabelCreateView, LabelUpdateView, LabelDelete
 urlpatterns = [
     path('', LabelIndexView.as_view(), name='index'),
     path('create/', LabelCreateView.as_view(), name='create'),
-    path('<int:pk>/update', LabelUpdateView.as_view(), name='update'),
-    path('<int:pk>/delete', LabelDeleteView.as_view(), name='delete'),
+    path('<int:pk>/update/', LabelUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', LabelDeleteView.as_view(), name='delete'),
 ]
--- task_manager/task/urls.py
+++ task_manager/task/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', TaskIndexView.as_view(), name='index'),
     path('create/', TaskCreateView.as_view(), name='create'),
     path('<int:pk>/', TaskDetailView.as_view(), name='detail'),
-    path('<int:pk>/update', TaskUpdateView.as_view(), name='update'),
-    path('<int:pk>/delete', TaskDeleteView.as_view(), name='delete'),
+    path('<int:pk>/update/', TaskUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', TaskDeleteView.as_view(), name='delete'),
 ]
